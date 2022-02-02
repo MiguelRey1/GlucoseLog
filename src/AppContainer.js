4947d9149244cd77fd9e9a8d3a5350a4461f551f
Miguel E. Reynoso <miguelrey003@gmail.com>
2022-02-02 15:44:46 -0400

    deleted comments and functionality useless

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,7 +1,7 @@
 import React, { Component, createRef } from "react";
 import FormsInputs from "./Components/FormsInputs";
 import TableData from "./Components/TableData";
-import './AppContainer.css';
+import "./AppContainer.css";
 
 const LogBook = [
   {
@@ -40,7 +40,6 @@ const LogBook = [
     },
     medicamento: "insulina Levemir",
   },
-  
 ];
 
 class AppContainer extends Component {
@@ -55,126 +54,98 @@ class AppContainer extends Component {
     this.txtMedicamento = createRef();
 
     this.state = {
-      data: [...LogBook] ,
-      // objEdited: null,
-      dataChange:{
-      id: LogBook.length+1,
-      fecha: new Date().toLocaleDateString() ,
-      hora: new Date().toLocaleTimeString(undefined,{hour:'numeric', minute:'numeric'}),
-      desayuno: {
-        antes: null,
-        despues: null
+      data: [...LogBook],
+      dataChange: {
+        id: LogBook.length + 1,
+        fecha: new Date().toLocaleDateString(),
+        hora: new Date().toLocaleTimeString(undefined, {
+          hour: "numeric",
+          minute: "numeric",
+        }),
+        desayuno: {
+          antes: null,
+          despues: null,
+        },
+        almuerzo: {
+          antes: null,
+          despues: null,
+        },
+        cena: {
+          antes: null,
+          despues: null,
+        },
+        medicamento: "",
       },
-      almuerzo: {
-        antes: null,
-        despues: null
-      },
-      cena: {
-        antes: null,
-        despues: null
-      },
-      medicamento: ''
-    },
 
-    refs:{
-      desayuno: {
-        antes:  this.txtDesayunoAntes,
-        despues:  this.txtDesayunoDespues
+      refs: {
+        desayuno: {
+          antes: this.txtDesayunoAntes,
+          despues: this.txtDesayunoDespues,
+        },
+        almuerzo: {
+          antes: this.txtAlmuerzoAntes,
+          despues: this.txtAlmuerzoDespues,
+        },
+        cena: {
+          antes: this.txtCenaAntes,
+          despues: this.txtCenaDespues,
+        },
+        medicamento: this.txtMedicamento,
       },
-      almuerzo: {
-        antes:  this.txtAlmuerzoAntes,
-        despues:  this.txtAlmuerzoDespues
-      },
-      cena: {
-        antes:  this.txtCenaAntes,
-        despues:  this.txtCenaDespues
-      },
-      medicamento:  this.txtMedicamento
-    },
-    objID: null
+      objID: null,
     };
-    
   }
-  componentDidMount() {
-    
-  }
+  componentDidMount() {}
 
-  add =(obj)=>{
-    /* this.setState({
-      data: [...obj]
-    }) */
-    /* this.setState({
-      dataChange:{
-        id: LogBook.length+1,
-        fecha: "09/01/22",
-        hora: "9:09",
+  handleAdd = (obj) => {
+    this.setState({
+      dataChange: {
+        id: this.state.dataChange.id + 1,
+        fecha: new Date().toLocaleDateString(),
+        hora: new Date().toLocaleTimeString(undefined, {
+          hour: "numeric",
+          minute: "numeric",
+        }),
         desayuno: {
           antes: obj.desayuno.antes,
-          despues: obj.desayuno.despues
+          despues: obj.desayuno.despues,
         },
         almuerzo: {
           antes: obj.almuerzo.antes,
-          despues: obj.almuerzo.despues
+          despues: obj.almuerzo.despues,
         },
         cena: {
           antes: obj.cena.antes,
-          despues: obj.cena.despues
+          despues: obj.cena.despues,
         },
-        medicamento: 'Levemir'
-        
-      }
-    })
-    LogBook.push(this.state.dataChange) */
-
-  }
-
-  handleAdd = (obj) =>{
-  this.setState({
-    dataChange:{
-      id: this.state.dataChange.id+1,
-      fecha: new Date().toLocaleDateString() ,
-      hora: new Date().toLocaleTimeString(undefined,{hour:'numeric', minute:'numeric'}),
-      desayuno: {
-        antes: obj.desayuno.antes,
-        despues: obj.desayuno.despues
+        medicamento: obj.medicamento,
       },
-      almuerzo: {
-        antes: obj.almuerzo.antes,
-        despues: obj.almuerzo.despues
-      },
-      cena: {
-        antes: obj.cena.antes,
-        despues: obj.cena.despues
-      },
-      medicamento: obj.medicamento
-    },
-  })
-    this.state.data.push(this.state.dataChange)
-  }
+    });
+    this.state.data.push(this.state.dataChange);
+  };
 
-  Edit = (e)=>{
+  Edit = (e) => {
     let id = Number.parseInt(e.target.id);
-      this.state.data.forEach((element) => {
-        if(element.id === id){
-          this.setState({
-            objID: element.id
-          })
-          this.txtDesayunoAntes.current.value = element.desayuno.antes
-          this.txtDesayunoDespues.current.value = element.desayuno.despues
-          this.txtAlmuerzoAntes.current.value = element.almuerzo.antes
-          this.txtAlmuerzoDespues.current.value = element.almuerzo.despues
-          this.txtCenaAntes.current.value = element.cena.antes
-          this.txtCenaDespues.current.value = element.cena.despues
-          this.txtMedicamento.current.value = element.medicamento
-        }
-      });
+    this.state.data.forEach((element) => {
+      if (element.id === id) {
+        this.setState({
+          objID: element.id,
+        });
+        this.txtDesayunoAntes.current.value = element.desayuno.antes;
+        this.txtDesayunoDespues.current.value = element.desayuno.despues;
+        this.txtAlmuerzoAntes.current.value = element.almuerzo.antes;
+        this.txtAlmuerzoDespues.current.value = element.almuerzo.despues;
+        this.txtCenaAntes.current.value = element.cena.antes;
+        this.txtCenaDespues.current.value = element.cena.despues;
+        this.txtMedicamento.current.value = element.medicamento;
+      }
+    });
+  };
 
-  }
-
-  handleEdit=(e)=> {
+  handleEdit = (e) => {
     let lista = this.state.data.map((element) => {
-      if(element.id === this.state.objID){
-        element.desayuno.antes =  this.txtDesayunoAntes.current.value;
+      if (element.id === this.state.objID) {
+        element.desayuno.antes = this.txtDesayunoAntes.current.value;
         element.desayuno.despues = this.txtDesayunoDespues.current.value;
         element.almuerzo.antes = this.txtAlmuerzoAntes.current.value;
         element.almuerzo.despues = this.txtAlmuerzoDespues.current.value;
@@ -182,50 +153,53 @@ class AppContainer extends Component {
         element.cena.despues = this.txtCenaDespues.current.value;
         element.medicamento = this.txtMedicamento.current.value;
       }
-      return element
+      return element;
     });
-      console.log(...lista)
-      this.setState({
-        data: [...lista]
-      })
-      this.txtDesayunoAntes.current.value = "";
-      this.txtDesayunoDespues.current.value = "";
-      this.txtAlmuerzoAntes.current.value = "";
-      this.txtAlmuerzoDespues.current.value = "";
-      this.txtCenaAntes.current.value = "";
-      this.txtCenaDespues.current.value = "";
-      this.txtMedicamento.current.value = "";
-
-  }
+    console.log(...lista);
+    this.setState({
+      data: [...lista],
+    });
+    this.txtDesayunoAntes.current.value = "";
+    this.txtDesayunoDespues.current.value = "";
+    this.txtAlmuerzoAntes.current.value = "";
+    this.txtAlmuerzoDespues.current.value = "";
+    this.txtCenaAntes.current.value = "";
+    this.txtCenaDespues.current.value = "";
+    this.txtMedicamento.current.value = "";
+  };
 
   handleDelete = (e) => {
-      let id = Number.parseInt(e.target.id);
-      this.state.data.forEach((element) => {
-        if(element.id === id){
-          return  this.state.data.splice(this.state.data.indexOf(element),1)
-        }
-      });
-      
-      this.setState({
-        data: this.state.data
-      })
-  }
+    let id = Number.parseInt(e.target.id);
+    this.state.data.forEach((element) => {
+      if (element.id === id) {
+        return this.state.data.splice(this.state.data.indexOf(element), 1);
+      }
+    });
 
+    this.setState({
+      data: this.state.data,
+    });
+  };
 
   render() {
     return (
       <div className="container">
         <section className="container-2">
-          <FormsInputs 
-          object={this.state.dataChange} 
-          myRefs={this.state.refs} 
-          ID={this.state.objID} 
-          data={this.state.data} 
-          handleEdit={this.handleEdit}  
-          handleAdd={this.handleAdd}/>
+          <FormsInputs
+            object={this.state.dataChange}
+            myRefs={this.state.refs}
+            ID={this.state.objID}
+            data={this.state.data}
+            handleEdit={this.handleEdit}
+            handleAdd={this.handleAdd}
+          />
         </section>
         <section className="container-table">
-          <TableData onHandleDelete={this.handleDelete} onHandleEdit={this.Edit} dataLog={this.state.data} />
+          <TableData
+            onHandleDelete={this.handleDelete}
+            onHandleEdit={this.Edit}
+            dataLog={this.state.data}
+          />
         </section>
       </div>
     );
--- src/Components/FormsInputs.js
+++ src/Components/FormsInputs.js
@@ -1,9 +1,8 @@
-import React, { useRef } from "react";
+import React from "react";
 import Buttons from "./Buttons";
 import Inputs from "./Inputs";
 
 const FormsInputs = (props) => {
-  
   const handleSubmit = (e) => {
     e.preventDefault();
   };
@@ -11,32 +10,34 @@ const FormsInputs = (props) => {
   const addData = () => {
     let obj = {
       desayuno: {
-        antes: props.object.desayuno.antes = props.myRefs.desayuno.antes.current.value,
-        despues: props.object.desayuno.despues = props.myRefs.desayuno.despues.current.value
+        antes: (props.object.desayuno.antes =
+          props.myRefs.desayuno.antes.current.value),
+        despues: (props.object.desayuno.despues =
+          props.myRefs.desayuno.despues.current.value),
       },
       almuerzo: {
-        antes: props.object.almuerzo.antes = props.myRefs.almuerzo.antes.current.value,
-        despues: props.object.almuerzo.despues = props.myRefs.almuerzo.despues.current.value
+        antes: (props.object.almuerzo.antes =
+          props.myRefs.almuerzo.antes.current.value),
+        despues: (props.object.almuerzo.despues =
+          props.myRefs.almuerzo.despues.current.value),
       },
       cena: {
-        antes: props.object.cena.antes = props.myRefs.cena.antes.current.value,
-        despues: props.object.cena.despues = props.myRefs.cena.despues.current.value
+        antes: (props.object.cena.antes =
+          props.myRefs.cena.antes.current.value),
+        despues: (props.object.cena.despues =
+          props.myRefs.cena.despues.current.value),
       },
-      medicamento: props.object.medicamento = props.myRefs.medicamento.current.value
+      medicamento: (props.object.medicamento =
+        props.myRefs.medicamento.current.value),
     };
-    props.myRefs.desayuno.antes.current.value= "";
-    props.myRefs.desayuno.despues.current.value= "";
-    props.myRefs.almuerzo.antes.current.value= "";
-    props.myRefs.almuerzo.despues.current.value= "";
-    props.myRefs.cena.antes.current.value= "";
-    props.myRefs.cena.despues.current.value= "";
-    props.myRefs.medicamento.current.value= "";
+    props.myRefs.desayuno.antes.current.value = "";
+    props.myRefs.desayuno.despues.current.value = "";
+    props.myRefs.almuerzo.antes.current.value = "";
+    props.myRefs.almuerzo.despues.current.value = "";
+    props.myRefs.cena.antes.current.value = "";
+    props.myRefs.cena.despues.current.value = "";
+    props.myRefs.medicamento.current.value = "";
     props.handleAdd(obj);
-  };
-
-  const edit = (e) => {
-  
-    
   };
 
   return (
@@ -86,9 +87,12 @@ const FormsInputs = (props) => {
           />
           <div>
             <Buttons cssProps="Buttons " title="Add" onClick={addData} />
-            <Buttons cssProps="Buttons " title="Modify" onClick={props.handleEdit} />
+            <Buttons
+              cssProps="Buttons "
+              title="Modify"
+              onClick={props.handleEdit}
+            />
           </div>
-          
         </div>
       </div>
     </form>
